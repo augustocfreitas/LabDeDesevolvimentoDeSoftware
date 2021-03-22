6d2442e0c0016c006da6355507318b9e1265bf62
Augusto Coutinho <47642058+augustocfreitas@users.noreply.github.com>
2021-03-22 10:49:05 -0300

    Cancelar Matricula View

---
+++ Sistema de Matrículas/src/sistemaDeMatriculas/Controller/CancelarMatriculaController.java
@@ -0,0 +1,33 @@
+package sistemaDeMatriculas.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+
+
+public class CancelarMatriculaController {
+
+    @FXML
+    private ChoiceBox<?> choiceBox_Curso;
+
+    @FXML
+    private ChoiceBox<?> choiceBox_Disciplina;
+
+    @FXML
+    private Button btn_Sair;
+
+    @FXML
+    private Button btn_Desmatricular;
+
+    @FXML
+    void action_DesmatricularAlunoEmDisciplina(ActionEvent event) {
+
+    }
+
+    @FXML
+    void action_VoltarAreaDoAluno(ActionEvent event) {
+
+    }
+
+}
