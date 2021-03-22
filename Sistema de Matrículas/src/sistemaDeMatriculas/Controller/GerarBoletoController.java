3ffb7b4efc6e14cb700513473e22f9ca781cb2f5
Augusto Coutinho <47642058+augustocfreitas@users.noreply.github.com>
2021-03-22 10:54:37 -0300

    Gerar Boleto View

---
+++ Sistema de Matrículas/src/sistemaDeMatriculas/Controller/GerarBoletoController.java
@@ -0,0 +1,22 @@
+package sistemaDeMatriculas.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+
+public class GerarBoletoController {
+
+    @FXML
+    private Button btn_Sair;
+
+    @FXML
+    private TextArea textArea_CodigoDoBoleto;
+
+    @FXML
+    void action_VoltarAreaDoAluno(ActionEvent event) {
+
+    }
+
+}
