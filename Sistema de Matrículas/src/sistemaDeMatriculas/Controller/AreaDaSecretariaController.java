7fdb7b7e16fa35160995b752b6c5d337afa032d5
Augusto Coutinho <47642058+augustocfreitas@users.noreply.github.com>
2021-03-22 10:25:36 -0300

    SecretariaView

---
+++ Sistema de Matrículas/src/sistemaDeMatriculas/Controller/AreaDaSecretariaController.java
@@ -0,0 +1,65 @@
+package sistemaDeMatriculas.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class AreaDaSecretariaController {
+
+    @FXML
+    private Button btn_Sair;
+
+    @FXML
+    private Button btn_CadastrarAluno;
+
+    @FXML
+    private Button btn_CadastrarProfessor;
+
+    @FXML
+    private Button btn_CadastrarCurso;
+
+    @FXML
+    private Button btn_GerarCurriculo;
+
+    @FXML
+    private Button btn_ListarAlunos3;
+
+    @FXML
+    private Button btn_GerarBoleto;
+
+    @FXML
+    void action_CadastrarAluno(ActionEvent event) {
+
+    }
+
+    @FXML
+    void action_CadastrarCurso(ActionEvent event) {
+
+    }
+
+    @FXML
+    void action_CadastrarDisciplina(ActionEvent event) {
+
+    }
+
+    @FXML
+    void action_CadastrarProfessor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void action_GerarBoleto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void action_GerarCurriculo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void action_VoltarAreaDeLogin(ActionEvent event) {
+
+    }
+
+}
