219645081f67621a74c789e4ea9f452b3827e4ba
Augusto Coutinho <47642058+augustocfreitas@users.noreply.github.com>
2021-03-22 10:33:40 -0300

    AreaDoAlunoView

---
+++ Sistema de Matrículas/src/sistemaDeMatriculas/Controller/AreaDoAlunoController.java
@@ -0,0 +1,41 @@
+package sistemaDeMatriculas.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class AreaDoAlunoController {
+
+    @FXML
+    private Button btn_Sair;
+
+    @FXML
+    private Button btn_MatricularEmDisciplina;
+
+    @FXML
+    private Button btn_CancelarMatricula;
+
+    @FXML
+    private Button btn_GerarBoleto;
+
+    @FXML
+    void action_CancelarMatricula(ActionEvent event) {
+
+    }
+
+    @FXML
+    void action_GerarBoleto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void action_MatricularEmDisciplina(ActionEvent event) {
+
+    }
+
+    @FXML
+    void action_VoltarAreaDeLogin(ActionEvent event) {
+
+    }
+
+}
