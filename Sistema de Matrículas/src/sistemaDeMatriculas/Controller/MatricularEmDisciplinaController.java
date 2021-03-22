0b746365e847a49e288ab6f153be6403879caa52
Augusto Coutinho <47642058+augustocfreitas@users.noreply.github.com>
2021-03-22 10:43:23 -0300

    MatricularEmDisciplina View

---
+++ Sistema de Matrículas/src/sistemaDeMatriculas/Controller/MatricularEmDisciplinaController.java
@@ -0,0 +1,34 @@
+package sistemaDeMatriculas.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableView;
+
+
+public class MatricularEmDisciplinaController {
+
+    @FXML
+    private ChoiceBox<?> choiceBox_Curso;
+
+    @FXML
+    private ChoiceBox<?> choiceBox_Disciplina;
+
+    @FXML
+    private Button btn_Sair;
+
+    @FXML
+    private Button btn_Matricular;
+
+    @FXML
+    void action_MatricularAlunoEmDisciplina(ActionEvent event) {
+
+    }
+
+    @FXML
+    void action_VoltarAreaDoAluno(ActionEvent event) {
+
+    }
+
+}
